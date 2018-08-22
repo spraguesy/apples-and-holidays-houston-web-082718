c3d94261332d92618bd8e4c07f0d284b68227ea3
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 01:17:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.map do |i, arr|
         if arr == "BBQ"
-          i
+          puts i
         end
     end
   end
